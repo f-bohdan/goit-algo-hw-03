7e6fddf444ca788c6741e0ded7c5aa5b0680322f
f-bohdan <fliakbohdan@gmail.com>
2025-01-21 20:24:59 +0000

    commit

--- vscode-basics/task2.py
+++ vscode-basics/task2.py
@@ -1,1 +1,15 @@
-task2
+import random
+
+
+def get_numbers_ticket(min, max, quantity):
+    numbers = []
+    # check for params
+    if min > 0 and max < 1000 and min < quantity < max:
+        # creating list of random numbers
+        numbers = random.sample(range(min, max), quantity)
+    # sorting
+    numbers.sort()
+    return numbers
+    
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
