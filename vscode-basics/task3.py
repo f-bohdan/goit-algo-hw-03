af86fbfe8dd5c7230390ec402dc57fb12aff1db1
f-bohdan <fliakbohdan@gmail.com>
2025-01-21 21:24:28 +0000

    commit

--- vscode-basics/task3.py
+++ vscode-basics/task3.py
@@ -1,17 +1,28 @@
 import re
 
-numbers = ["    +38(050)123-32-34",
-           "     0503451234",
-           "(050)8889900",
-           "38050-111-22-22",
-           "38050 111 22 11   ",
-]
-
-def normalize_phone(phone_number):
-    phone_number = re.findall("\d+", phone_number)
+def normalize_phone(phone_number: str):
+    # deleta all symbols except + and numbers
+    phone_number = re.sub(r"[^0-9]", '', phone_number)
+    # cheching first symbols in string
+    if phone_number.startswith("38"):
+        phone_number = "+"+phone_number
+    elif phone_number.startswith("+38"):
+        pass
+    else:
+        phone_number = "+38"+phone_number
     return phone_number
 
+raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
 
-new_number = []
-for number in numbers:
-    new_number.append(normalize_phone(normalize_phone))
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
