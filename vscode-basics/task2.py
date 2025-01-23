9886f4032aded350bd9da762aa60a64223e4edae
f-bohdan <fliakbohdan@gmail.com>
2025-01-23 22:47:30 +0000

    commit

--- vscode-basics/task2.py
+++ vscode-basics/task2.py
@@ -4,12 +4,19 @@ import random
 def get_numbers_ticket(min, max, quantity):
     numbers = []
     # check for params
-    if min > 0 and max < 1000 and min < quantity < max:
+    if 0 < min < max < 1000 and 0 < quantity <=(max-min):
         # creating list of random numbers
         numbers = random.sample(range(min, max), quantity)
+    elif min > max:
+        print("Вказано не вірне значення MIN")
+        return None
+    elif not (0<quantity <=(max-min)):
+        print("Вказано не вірне значення QUANITY")
+        return None
+        
     # sorting
     numbers.sort()
     return numbers
     
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(4, 8, 4)
 print("Ваші лотерейні числа:", lottery_numbers)
